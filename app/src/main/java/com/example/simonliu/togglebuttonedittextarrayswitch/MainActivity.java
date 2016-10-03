178c5e8c868ee4a5403b1f8906ef803552eebc06
simonliu009 <toopooliu@gmail.com>
2016-10-04 02:14:26 +0800

    First Pass of Lamp Control

--- app/src/main/java/com/example/simonliu/togglebuttonedittextarrayswitch/MainActivity.java
+++ app/src/main/java/com/example/simonliu/togglebuttonedittextarrayswitch/MainActivity.java
@@ -6,30 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import static com.example.simonliu.togglebuttonedittextarrayswitch.R.id.radioGroup1;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private ToggleButton toggleButton1, toggleButton2;
-    private Button displayButton;
-    private Button editTextButton;
-    private EditText inputText;
+    private Button powerOnButton;
+    private Button shutdownButton;
 
     public int delayMinutes = 0;
     public int delaySeconds = 0;
     public int delayResults = 0;
+    public String modeMSG = "";
+
+
 //
 //    private static final Integer[] delayOptions={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60};
 //    private TextView minutesView;
@@ -44,22 +40,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         addListenerOnButton();
         addListenerOnRadioButton();
-        addListenerOnSwitch();
+        modeMSG = getRadioButton1Text();
 
-
-        String autoText[] = {"Baidu","Google","Bing","Yahoo","Sogou","Sohu","Sina"};
-        AutoCompleteTextView t1 = (AutoCompleteTextView) findViewById(R.id.auto_Complete_TextView);
-        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, autoText);
-        t1.setThreshold(1);
-        t1.setAdapter(adp);
 
         //        Integer[] delayOptions = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59}
-
-
 //res.getIntArray(R.array.delaySettingsArray) is the int[] array get from resources but we need to convert it to Integer[]
         Resources res = getResources();
-        final   Integer[] delayOptions = new Integer[res.getIntArray(R.array.delaySettingsArray).length];
-        for (int i = 0; i<res.getIntArray(R.array.delaySettingsArray).length; i++){
+        final Integer[] delayOptions = new Integer[res.getIntArray(R.array.delaySettingsArray).length];
+        for (int i = 0; i < res.getIntArray(R.array.delaySettingsArray).length; i++) {
             delayOptions[i] = Integer.valueOf(res.getIntArray(R.array.delaySettingsArray)[i]);
         }
 
@@ -83,9 +71,9 @@ public class MainActivity extends AppCompatActivity {
 
                 delayMinutes = Integer.valueOf(delayOptions[i]);
                 delayResults = delayMinutes * 60 + delaySeconds;
-                Toast.makeText(MainActivity.this, "Delay Setting for Minutes " + delayMinutes + " Minutes", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Delay Setting for Seconds: " + delaySeconds + " Seconds", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Delay Final Results: " + delayResults + " Seconds in Total", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Setting for Minutes " + delayMinutes + " Minutes", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Setting for Seconds: " + delaySeconds + " Seconds", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Final Results: " + delayResults + " Seconds in Total", Toast.LENGTH_SHORT).show();
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -103,9 +91,9 @@ public class MainActivity extends AppCompatActivity {
 //                delaySeconds = i;
                 delaySeconds = Integer.valueOf(delayOptions[i]);
                 delayResults = delayMinutes * 60 + delaySeconds;
-                Toast.makeText(MainActivity.this, "Delay Setting for Minutes " + delayMinutes + " Minutes", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Delay Setting for Seconds: " + delaySeconds + " Seconds", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Delay Final Results: " + delayResults + " Seconds in Total", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Setting for Minutes " + delayMinutes + " Minutes", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Setting for Seconds: " + delaySeconds + " Seconds", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Delay Final Results: " + delayResults + " Seconds in Total", Toast.LENGTH_SHORT).show();
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -115,56 +103,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     protected void onStart() {
         super.onStart();
 
     }
 
 
-
-
-
-    public void addListenerOnButton (){
-        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
-        toggleButton2 = (ToggleButton) findViewById(R.id.toggleButton2);
-        displayButton = (Button) findViewById(R.id.btnDisplay);
-        editTextButton = (Button) findViewById(R.id.edit_text_button);
-        inputText = (EditText) findViewById(R.id.edittext);
-
-        displayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StringBuffer displayMessage = new StringBuffer();
-                displayMessage.append("Toggle Button 1: ").append(toggleButton1.getText());
-                displayMessage.append("\nToggle Button 2: ").append(toggleButton2.getText());
-                int duration = Toast.LENGTH_LONG;
-                Toast finalDisplay = Toast.makeText(MainActivity.this, displayMessage, duration);
-                finalDisplay.show();
-//                Toast.makeText(MainActivity.this, displayMessage, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        editTextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputString = inputText.getText().toString();
-                int duration = Toast.LENGTH_SHORT;
-                Toast inputTextDisplay = Toast.makeText(MainActivity.this, inputString, duration);
-                inputTextDisplay.show();
-            }
-        });
+    public String getRadioButton1Text() {
+        RadioButton rb = (RadioButton) findViewById(R.id.radioButton1);
+        String modeMSG = rb.getText().toString();
+        return modeMSG;
     }
 
 
-    public void addListenerOnRadioButton(){
+    public void addListenerOnButton() {
+        powerOnButton = (Button) findViewById(R.id.powerOnButtonId);
+        shutdownButton = (Button) findViewById(R.id.shutdowButtonId);
+
+        powerOnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String powerOnMessage = getString(R.string.powerOnMSG) + modeMSG;
+                Toast.makeText(MainActivity.this, powerOnMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        shutdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String shutdownMessage = getString(R.string.showdownMSG) + " " + delayMinutes + " " + getString(R.string.minutes) + " " + delaySeconds + " " + getString(R.string.seconds);
+                Toast.makeText(MainActivity.this, shutdownMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+//Another way to get the checkedID text to String
+//    RadioGroup rb = (RadioGroup)findViewById(R.id.radioGroup1);
+//    modeMSG = ((RadioButton)findViewById(rb.getCheckedRadioButtonId())).getText().toString();
+
+    public void addListenerOnRadioButton() {
         RadioGroup radioGroupPi = (RadioGroup) findViewById(radioGroup1);
-        radioGroupPi.clearCheck();
         radioGroupPi.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton rb = (RadioButton) findViewById(checkedId);
-                if (null!= rb && checkedId > -1) {
+                if (null != rb && checkedId > -1) {
+                    modeMSG = rb.getText().toString();
                     Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
                 }
             }
@@ -172,62 +158,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//Missing (View view) will cause crash
-    public void clearButton(View view){
-        RadioGroup radioGroupPi = (RadioGroup) findViewById(radioGroup1);
-        radioGroupPi.clearCheck();
-    }
 
-
-    public void delayShutdownSubmit(View view){
-//       Button delayShutdownButton = (Button) findViewById(delayShutdowButtonId);
-        Toast.makeText(MainActivity.this, "Delay Setting for Minutes " + delayMinutes + " Minutes", Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "Delay Setting for Seconds: " + delaySeconds + " Seconds", Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "Delay Final Results: " + delayResults + " Seconds in Total", Toast.LENGTH_SHORT).show();
-    }
-
-
-    public void addListenerOnSwitch(){
-        Switch lampSwitch1 = (Switch) findViewById(R.id.switch_1);
-        lampSwitch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    Toast.makeText(MainActivity.this, R.string.switch_1_On, Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, R.string.switch_off, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        Switch lampSwitch2 = (Switch) findViewById(R.id.switch_2);
-        lampSwitch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    Toast.makeText(MainActivity.this, R.string.switch_2_on, Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, R.string.switch_off, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-
-        Switch lampSwitch3 = (Switch) findViewById(R.id.switch_3);
-        lampSwitch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    Toast.makeText(MainActivity.this, R.string.switch_3_on, Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, R.string.switch_off, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
+//    public void shutdownSubmit(View view) {
+////       Button delayShutdownButton = (Button) findViewById(delayShutdowButtonId);
+//        String shutdownMessage = getString(R.string.showdownMSG) + delayMinutes + " Minutes" + delaySeconds + " Seconds";
+//        Toast.makeText(MainActivity.this, shutdownMessage, Toast.LENGTH_SHORT).show();
+//
+//    }
 
 }
+
+
