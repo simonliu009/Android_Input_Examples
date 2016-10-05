2755f5fe0eb50238cd50905aafcb54511badb534
simonliu009 <toopooliu@gmail.com>
2016-10-05 14:19:31 +0800

    Update the auto versioncode code in build.gradle (Module:app)

--- app/src/main/java/com/example/simonliu/togglebuttonedittextarrayswitch/MainActivity.java
+++ app/src/main/java/com/example/simonliu/togglebuttonedittextarrayswitch/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.simonliu.togglebuttonedittextarrayswitch;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -53,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButton();
         addListenerOnRadioButton();
         modeMSG = getDefaultCheckedRBText();
+        displayVersionName();
 
 
         //     Integer[] delayOptions = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59}
@@ -353,6 +358,39 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+
+    //版本名
+    public static String getVersionName(Context context) {
+        return getPackageInfo(context).versionName;
+    }
+
+
+    //版本号
+    public static int getVersionCode(Context context) {
+        return getPackageInfo(context).versionCode;
+    }
+
+    private static PackageInfo getPackageInfo(Context context) {
+        PackageInfo pi = null;
+
+        try {
+            PackageManager pm = context.getPackageManager();
+            pi = pm.getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_CONFIGURATIONS);
+
+            return pi;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pi;
+    }
+
+    public void displayVersionName() {
+        TextView versionName = (TextView) findViewById(R.id.version_name_id);
+        String version1 = getResources().getString(R.string.version_code) + "  " + getVersionCode(MainActivity.this);
+        versionName.setText(version1);
+    }
 
 //    public void shutdownSubmit(View view) {
 ////       Button delayShutdownButton = (Button) findViewById(delayShutdowButtonId);
